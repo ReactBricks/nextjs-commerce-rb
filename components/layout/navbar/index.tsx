c9ecc6b61df5e51d72643539323d82dcb8e22bfb
Dario Ronzoni <dario.ronzoni@f2.net>
2024-07-29 17:26:25 +0200

    feat: show menu items from "main-menu" collection

--- components/layout/navbar/index.tsx
+++ components/layout/navbar/index.tsx
@@ -10,7 +10,8 @@ import Search, { SearchSkeleton } from './search';
 const { SITE_NAME } = process.env;
 
 export async function Navbar() {
-  const menu = await getMenu('next-js-frontend-header-menu');
+  // const menu = await getMenu('next-js-frontend-header-menu');
+  const menu = await getMenu('main-menu');
 
   return (
     <nav className="relative flex items-center justify-between p-4 lg:px-6">
