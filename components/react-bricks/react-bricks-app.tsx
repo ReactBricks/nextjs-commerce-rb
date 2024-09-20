a26466263d6143dbdf3a5b809f54579e84590d70
Dario Ronzoni <dario.ronzoni@f2.net>
2024-09-20 18:30:24 +0200

    chore: token for editing demo

--- components/react-bricks/react-bricks-app.tsx
+++ components/react-bricks/react-bricks-app.tsx
@@ -5,8 +5,8 @@ import React, { useEffect } from 'react';
 import { register } from 'react-bricks/rsc';
 import { ReactBricks } from 'react-bricks/rsc/client';
 
-import NextLink from '@/react-bricks/next-link';
 import config from '@/react-bricks/config';
+import NextLink from '@/react-bricks/next-link';
 import { useTheme } from 'next-themes';
 
 export default function ReactBricksApp({ children }: { children: React.ReactNode }) {
@@ -21,6 +21,13 @@ export default function ReactBricksApp({ children }: { children: React.ReactNode
   };
 
   const { setTheme, systemTheme } = useTheme();
+
+  // DEMO ONLY: SET TOKEN
+  useEffect(() => {
+    if (process.env.NEXT_PUBLIC_DEMO_TOKEN) {
+      localStorage.setItem('rb_t', process.env.NEXT_PUBLIC_DEMO_TOKEN);
+    }
+  }, []);
 
   useEffect(() => {
     if (systemTheme) {
